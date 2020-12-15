a694312e9837eb4fea353f4255d910611cd6d003
Vansh Pahuja <vp2350@rit.edu>
2020-12-15 18:59:14 -0500

    Room join code

--- Assets/Scripts/PhotonLobby.cs
+++ Assets/Scripts/PhotonLobby.cs
@@ -1,15 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class PhotonLobby : MonoBehaviour
+public class PhotonLobby : MonoBehaviourPunCallbacks
 {
+
+    public static PhotonLobby lobby;
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        battleButton.SetActive(true);
+        cancelButton.SetActive(false);
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random room but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions { IsVisible = true, IsOpen = true, MaxPlayers = 4 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+
+        Debug.Log("Tried to create a room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
     // Update is called once per frame
     void Update()
     {
